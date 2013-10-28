d0eb4e967f198642fe06dc3766018463d2676b2b
boyska <piuttosto@logorroici.org>
2013-10-28 22:23:58 +0100

    Add extract_wiki to tools/

---
+++ tools/README.txt
@@ -0,0 +1,2 @@
+This directory is intended as an archive of various tools that are useful for
+developers but are not part of the build process itself.
---
+++ tools/extract_wiki/extract.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import urljoin, urlsplit
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+
+def remove_js(content):
+    '''remove js inclusion'''
+    ### Bit of cleanup
+    scripts = content.find_all('script')
+    for s in scripts:
+        s.replace_with(Comment("script rimosso"))
+
+
+def download_url(pageurl, target):
+    imgdir = target + "_files"
+
+    def get_external(el, tag):
+        if not os.path.exists(imgdir):
+            os.mkdir(imgdir)
+        resource_url = urljoin(pageurl, el[tag])
+        base = urlsplit(resource_url).path.split('/')[-1]
+        target = os.path.join(imgdir, base)
+        if not os.path.exists(target):
+            urlretrieve(resource_url, filename=target)
+        el[tag] = target
+
+    p = BeautifulSoup(urlopen(pageurl))
+    head = p.find('head')
+    content = p.find(id='wiki_html')
+
+    remove_js(p)
+
+    ### Images
+    for image in content.findAll("img"):
+        get_external(image, "src")
+    for css in p.findAll("link"):
+        get_external(css, "href")
+
+    ### Finally, write to disk!
+    print('''<html>%(head)s<body>
+          <div class="content_box"><div class="wiki">%(body)s</div></div>
+          <div class="creation-notes">
+          This is a copy of <a href="%(url)s">%(url)s</a>, taken on %(date)s
+          </div>
+          </body></html>''' % {'head': head, 'body': content, 'url': pageurl,
+                               'date': datetime.now().strftime('%d %B %Y')},
+          file=open(target, 'w'))
+
+
+if __name__ == '__main__':
+    download_url(sys.argv[1], sys.argv[2])
